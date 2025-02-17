f7445bb64e0227eabf51e2f702508d191174463c
Reuben Atherton <reubenatherton@Reubens-MacBook-Air.local>
2025-02-17 18:26:02 +0000

    fully working with all legal pawn moves and captures shown with new icons

--- src/dimen/dimen.py
+++ src/dimen/dimen.py
@@ -2,15 +2,18 @@ import pygame as p
 
 WIDTH = HEIGHT = 512
 DIMENSION = 8
+SCALER = 100
 
 SQ_SIZE = HEIGHT // DIMENSION
 MAX_FPS = 15
 IMAGES = {}
+DOTS = {}
 
 BACKGROUND_COLOR = p.Color("white")
 SQUARE_COLOUR = p.Color("beige")
-SQUARE_COLOUR_2 = p.Color("palegreen4")
+SQUARE_COLOUR_2 = p.Color("turquoise4")
 SQUARE_SELECTED_COLOUR = p.Color("slategray3")
+SQUARE_VALID_MOVE_COLOUR = p.Color("honeydew2")
 
 # Colours for future choices
 # palegreen4
--- src/main/main.py
+++ src/main/main.py
@@ -1,8 +1,9 @@
 import pygame as p
 
-from src import engine
 from src.dimen.dimen import DIMENSION, IMAGES, WIDTH, HEIGHT, SQ_SIZE, MAX_FPS, BACKGROUND_COLOR, SQUARE_COLOUR, \
-    SQUARE_SELECTED_COLOUR, SQUARE_COLOUR_2
+    SQUARE_SELECTED_COLOUR, SQUARE_COLOUR_2, SCALER, DOTS
+from src.main.GameState import GameState
+from src.main.Move import Move
 
 
 def main():
@@ -10,7 +11,7 @@ def main():
     screen = p.display.set_mode((WIDTH, HEIGHT))
     clock = p.time.Clock()
     screen.fill(BACKGROUND_COLOR)
-    game_state = engine.GameState()
+    game_state = GameState()
     valid_moves = game_state.get_valid_moves()
     move_made = False # Flag variable for when a move is made
 
@@ -37,7 +38,7 @@ def main():
                     player_clicks.append(sq_selected)
 
                 if len(player_clicks) == 2:
-                    move = engine.Move(player_clicks[0], player_clicks[1], game_state.board)
+                    move = Move(player_clicks[0], player_clicks[1], game_state.board)
                     if move in valid_moves:
                         game_state.make_move(move)
                         move_made = True
@@ -53,7 +54,7 @@ def main():
             valid_moves = game_state.get_valid_moves()
             move_made = False
 
-        draw_game_state(screen, game_state, sq_selected)
+        draw_game_state(screen, game_state, sq_selected, valid_moves)
         clock.tick(MAX_FPS)
         p.display.flip()
 
@@ -65,31 +66,55 @@ def load_images():
     for piece in pieces:
         IMAGES[piece] = p.transform.scale(p.image.load(f"images/{piece}.png"), (SQ_SIZE, SQ_SIZE))
 
-def  draw_game_state(screen, game_state, sq_selected):
-    draw_board(screen, sq_selected)
-    draw_pieces(screen, game_state.board)
+    DOTS[0] = p.transform.scale(p.image.load("images/circle-move.png"),(SQ_SIZE // 2, SQ_SIZE // 2))
+    DOTS[1] = p.transform.scale(p.image.load("images/circle-capture.png"), (SQ_SIZE, SQ_SIZE))
 
-def draw_board(screen, sq_selected):
+def  draw_game_state(screen, game_state, sq_selected, valid_moves):
+    draw_board(screen, game_state.board, sq_selected, valid_moves)
+
+def draw_board(screen, board, sq_selected, valid_moves):
+    # Step 1: Draw chess board
     for row in range(DIMENSION):
         for col in range(DIMENSION):
-
-            # Set chess board colours
+            # Set board colours
             if (row + col) % 2 == 0:
                 colour = SQUARE_COLOUR
             else:
                 colour = SQUARE_COLOUR_2
 
+            # Highlight selected square
             if (row, col) == sq_selected:
                 colour = SQUARE_SELECTED_COLOUR
 
+            # Draw board and pieces
             p.draw.rect(screen, colour, p.Rect(col *SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+            draw_pieces(screen, board, row, col)
 
-def draw_pieces(screen, board):
-    for row in range(DIMENSION):
-        for col in range (DIMENSION):
+            # Draw top layer - valid moves, valid captures
+            if sq_selected:
+                for move in valid_moves:
+                    if (move.start_row, move.start_col) == sq_selected:
+                        if (row, col) == (move.end_row, move.end_col):
+
+                            # Empty tile
+                            if board[row][col] == "--":
+                                dot_x = col * SQ_SIZE + (SQ_SIZE - DOTS[0].get_width()) // 2
+                                dot_y = row * SQ_SIZE + (SQ_SIZE - DOTS[0].get_height()) // 2
+                                screen.blit(DOTS[0], (dot_x, dot_y))
+
+                            # Occupied tile
+                            else:
+                                dot_x = col * SQ_SIZE + (SQ_SIZE - DOTS[1].get_width())
+                                dot_y = row * SQ_SIZE + (SQ_SIZE - DOTS[1].get_height())
+                                screen.blit(DOTS[1], (dot_x, dot_y))
+
+def draw_pieces(screen, board, row, col):
             piece = board[row][col]
             if piece != "--":
-                screen.blit(IMAGES[piece], p.Rect(col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+                screen.blit(IMAGES[piece], p.Rect(col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE // SCALER, SQ_SIZE //SCALER))
+
+def draw_valid_moves():
+    pass
 
 if __name__ == "__main__":
     main()
